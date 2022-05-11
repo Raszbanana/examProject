f52a48f8bda73c6445880c07519c3d691ae26fa4
pstepien-design <pstepien@interia.pl>
2022-05-11 22:22:57 +0200

    finished login panel and added styling

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -5,59 +5,84 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { useDispatch } from 'react-redux';
 import { StackParamList } from '../typings/navigations';
-import { rehydrateUser,login } from '../store/actions/user.actions';
+import { rehydrateUser, login } from '../store/actions/user.actions';
 
-type ScreenNavigationType = NativeStackNavigationProp<
-    StackParamList,
-    "Login"
->
+type ScreenNavigationType = NativeStackNavigationProp<StackParamList, 'Login'>;
 
 export default function LoginScreen() {
-    const navigation = useNavigation<ScreenNavigationType>()
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const dispatch = useDispatch();
+  const navigation = useNavigation<ScreenNavigationType>();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
 
-    async function readPersistedUserInfo() {
-        const token = await SecureStore.getItemAsync('idToken');
-        const userJson = await SecureStore.getItemAsync('user');
-        let user = null;
-        if (userJson) {
-            user = JSON.parse(userJson);
-        }
-        if (user) {
-            // then we have a priv. login
-            // restore the signup by updating the redux store based on usre and token.
-            dispatch(rehydrateUser(user, token!))
-        }
+  async function readPersistedUserInfo() {
+    const token = await SecureStore.getItemAsync('idToken');
+    const userJson = await SecureStore.getItemAsync('user');
+    let user = null;
+    if (userJson) {
+      user = JSON.parse(userJson);
     }
+    if (user) {
+      // then we have a priv. login
+      // restore the signup by updating the redux store based on usre and token.
+      dispatch(rehydrateUser(user, token!));
+    }
+  }
 
-    useEffect(() => {
-        readPersistedUserInfo();
-    }, [])
-  
+  useEffect(() => {
+    readPersistedUserInfo();
+  }, []);
+
+
+
   return (
-      <View style={styles.container}>
-      <Text>Login Screen</Text>
-      <TextInput value={email} placeholder="email" onChangeText={setEmail} />
-      <TextInput value={password} placeholder="password" onChangeText={setPassword} />
-       <Button title="Login" onPress={() => dispatch(login(email, password))} /> 
-      <Text  style={styles.signup_text}>Don't you already have an account? Sign up!</Text>
-      <Button title="Go to signup page" onPress={() => navigation.navigate("Signup")} />
-  </View>
-    );
+    <View style={styles.container}>
+      <Text style={styles.login__text}>Login Screen</Text>
+      <TextInput style={styles.input} value={email} placeholder='email'  placeholderTextColor="#000" onChangeText={setEmail} />
+      <TextInput style={styles.input}
+        value={password}
+        placeholder='password'
+        onChangeText={setPassword}
+        placeholderTextColor="#000" 
+      />
+      <Button title='Login' onPress={() => dispatch(login(email, password))} />
+      <Text style={styles.signup_text}>
+        Don't you already have an account? Sign up!
+      </Text>
+      <Button
+        title='Go to signup page'
+        onPress={() => navigation.navigate('Signup')}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    signup_text: {
-      marginTop: 20,
-      textTransform: 'uppercase',
-      fontWeight: 'bold',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 150,
   },
-})  
+  signup_text: {
+    marginTop: 20,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  login__text: {
+    fontWeight: 'bold',
+    marginBottom: 50,
+    fontSize: 20,
+  },
+  input: {
+    borderColor: '#000000',
+    borderWidth: 1,
+    width: '80%',
+    height: 30,
+    color: 'black',
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+ 
+});
--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -19,12 +19,30 @@ export default function SignupScreen() {
     const [password, setPassword] = useState('');
     const dispatch = useDispatch();
 
+    async function readPersistedUserInfo() {
+        const token = await SecureStore.getItemAsync('idToken');
+        const userJson = await SecureStore.getItemAsync('user');
+        let user = null;
+        if (userJson) {
+            user = JSON.parse(userJson);
+        }
+        if (user) {
+            // then we have a priv. login
+            // restore the signup by updating the redux store based on usre and token.
+            dispatch(rehydrateUser(user, token!))
+        }
+    }
+
+    useEffect(() => {
+        readPersistedUserInfo();
+    }, [])
+
 
     return (
         <View style={styles.container}>
-            <Text>Signup Screen</Text>
-            <TextInput value={email} placeholder="email" onChangeText={setEmail} />
-            <TextInput value={password} placeholder="password" onChangeText={setPassword} />
+            <Text style={styles.signup_text}>Signup Screen</Text>
+            <TextInput style={styles.input} value={email} placeholder="email" placeholderTextColor="#000" onChangeText={setEmail} />
+            <TextInput style={styles.input} value={password} placeholder="password" placeholderTextColor="#000" onChangeText={setPassword} />
             <Button title="Signup" onPress={() => dispatch(signup(email, password))} />
             <Text  style={styles.login_text}>Do you already have an account? Log in !</Text>
             <Button title="Go to login page" onPress={() => navigation.navigate("Login")} />
@@ -37,11 +55,26 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
-    },
-    login_text: {
+        paddingTop: 150,
+      },
+      login_text: {
         marginTop: 20,
         textTransform: 'uppercase',
         fontWeight: 'bold',
-    },
+      },
+      signup_text: {
+        fontWeight: 'bold',
+        marginBottom: 50,
+        fontSize: 20,
+      },
+      input: {
+        borderColor: '#000000',
+        borderWidth: 1,
+        width: '80%',
+        height: 30,
+        color: 'black',
+        marginTop: 10,
+        marginBottom: 10,
+        textAlign: 'center',
+      },
 })
